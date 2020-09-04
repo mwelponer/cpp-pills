584054a2cc2144073fb29fc6dfadcd7f511a3f61
mike <welponer@fbk.eu>
2020-09-04 17:47:11 +0200

    codeLite struct added

---
+++ codeLite/struct/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*struct is just a class in which members are public by default whereas in cpp
+class members are private by default
+struct are there to maintain compatibility with c*/
+struct Player {
+	int x, y;
+	int speed;
+	
+	void move(int xa, int ya){
+		x += xa * speed;
+		y += ya * speed;
+	}
+};
+
+int main(int argc, char **argv)
+{
+	Player p;
+	p.move(1, -1);
+	//return 0;
+}
