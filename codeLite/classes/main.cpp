34974d6244ab61fffb13837cd015a95197314724
Mike <mwelponer@gmail.com>
2020-09-06 16:55:56 +0200

    xcode variables added

--- codeLite/classes/main.cpp
+++ codeLite/classes/main.cpp
@@ -2,18 +2,19 @@
 #include "Something.h"
 
 class Player {
-	
+
 private:
-	/*by convention use prefix m_ for private member variables
-	to distinguish them from local variables*/
+	/* by convention use prefix m_ for private member variables
+	to distinguish them from local variables */
 	int m_x, m_y;
 	int m_speed;
-	
+
 public:
-	
+
 	Player() : m_speed(0) {}
 
 	void move(int x, int y){
+		// this->m_x is like to write (*Player).m_x 
 		this->m_x += x * this->m_speed;
 		this->m_y += y * this->m_speed;
 	}
@@ -23,13 +24,13 @@ int main(int argc, char **argv)
 {
 	Player p;
 	p.move(1, -1);
-	
+
 	Something s;
 	std::cout << s.s_value << std::endl;
 	Something::s_value = 0;
 	std::cout << s.s_value << std::endl;
 	Something::s_function();
 	std::cout << s.s_value << std::endl;
-	
+
 	//return 0;
 }
