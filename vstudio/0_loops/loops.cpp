25757b108a65b26f5c98cd35a7487049cfb16607
Mike <mwelponer@gmail.com>
2022-08-06 01:50:29 +0200

    vstudio added loops, classes, structs

---
+++ vstudio/0_loops/loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    for(int i=0; i<10; i++)
+        std::cout << i << std::endl;
+
+    bool condition = false;
+
+    // it will run only if condition is true
+    while(condition)
+        std::cout << "while" << std::endl;
+
+    // it will run at least once, then it will evaluate condition
+    do{
+        std::cout << "do while" << std::endl;
+    }while(condition);
+}
---
+++ vstudio/4_structs/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* struct is just a class in which members are public by default whereas in cpp
+class members are private by default
+struct are there to maintain compatibility with c
+struct are used mainly for plain data, like for example definition of a 2dVector 
+that contains just two float variables. I will not use struct where there is inheritence */
+struct Player {
+	int x, y;
+	int speed;
+	
+	void move(int xa, int ya){
+		x += xa * speed;
+		y += ya * speed;
+	}
+};
+
+int main(int argc, char **argv)
+{
+	Player p;
+	p.move(1, -1);
+	return 0;
+}
