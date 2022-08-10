b95918e86ca38afb7ef147ed2a86e7d84d89a703
mike <welponer@fbk.eu>
2022-08-10 14:46:00 +0200

    vstudio added static, staticInClasses, enum, virtualFunctions

--- vstudio/3_classes/main.cpp
+++ vstudio/3_classes/main.cpp
@@ -17,7 +17,13 @@ public:
 		//m_name = "Unknown"; // standard way to initialize class members (less efficient!)
 		//m_age = 0;
 		
-		std::cout << "entity created" << std::endl;
+		std::cout << "Entity created - " << this->m_name << std::endl;
+	}
+
+	~Entity() 
+	{
+		std::cout << "Entity destroyed! - " << this->m_name << std::endl;
+		/* here you clean any memory that you have used*/
 	}
 	
 	const std::string& getName() const {
@@ -44,13 +50,13 @@ public:
 	Player() :
 		m_speed(0) 
 	{
-		std::cout << "Player created.." << std::endl;
+		std::cout << "Player created - " << this->getName() << std::endl;
 	}
 	
 	~Player() {
 		/* nothing to be deallocated here
 		cause we just have m_speed that is allocated in-place (without a class) */
-		std::cout << "Player destroyed!" << std::endl;
+		std::cout << "Player destroyed! - " << this->getName() << std::endl;
 	}
 
 	void move(int x, int y){
@@ -64,30 +70,36 @@ int main(int argc, char **argv)
 {
 	//////////////////////////////////
     // allocating on the STACK !!!
+	// the destructor ~Player() gets automatically called at the end of the scope!
+	{
 	Player p;
 	p.move(1, -1);
 	
 	// check if inheritance works
-	std::cout << p.getName() << std::endl;
+	std::cout << "get Name: " << p.getName() << std::endl;
 	p.setName("mike");
-	std::cout << p.getName() << std::endl;
+	std::cout << "get Name: " << p.getName() << std::endl;
+	}
 
+	std::cout << " " << std::endl;
 
 	//////////////////////////////////
     // allocating on the HEAP !!!
-    Player* ptr = new Player;
+	// I need to manually call the destructor!
+    Player* ptr = new Player();
+	ptr->setName("pippo");
     ptr->move(1, -1);
     /* the arrow -> means dereference the pointer (*ptr) and then
     I can use the object member */
     Player& player = *ptr;
     player.move(1, -1); // OR (*ptr).move(1, -1);
     
-	delete ptr;
+	delete ptr; // manually call the destructor
 	
 	Something s;
-	std::cout << s.s_value << std::endl;
+	std::cout << "s_value: " << s.s_value << std::endl;
 	Something::s_value = 0;
-	std::cout << s.s_value << std::endl;
+	std::cout << "s_value: " << s.s_value << std::endl;
 	Something::s_function();
-	std::cout << s.s_value << std::endl;
+	std::cout << "s_value: " << s.s_value << std::endl;
 }
---
+++ vstudio/5_static/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int s_var = 10;
+//int var = 8; //this is not allowed: the linker finds multiple definition of var
+
+extern int nother; // extern means find the value of that variable in some other translation unit
+extern int s_nother; 
+
+//void function() {} // this is not working because function() is already declared in static.cpp
+
+/* but s_function() can be declared as in static.cpp the s_function() is
+declared static */
+void s_function() {}
+
+/* if i use static variable inside myFunction, the first time we run the
+function it declares the variable and initialize it to 0, the other times 
+it just refer to the original variable to increment it 
+So it's like declaring the variable outside the function, but in this way
+the variable can be modified (incremented) only inside the scope of myFunction */
+void myFunction() {
+	static int i = 0;
+	i++;
+	std::cout << "i: " << i << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+	std::cout << "s_var: " << s_var << std::endl;
+	std::cout << "nother: " << nother << std::endl;
+
+	/* this is not working 
+	(undefined reference to s_nother) because s_another in static.cpp is declared static */
+	//std::cout << "s_nother: " << s_nother << std::endl; 
+	
+	myFunction();
+	myFunction();
+	myFunction();
+}
