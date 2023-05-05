9f98d3560d711376e7d2771cf4301d9b2617edb9
mike <welponer@fbk.eu>
2023-05-05 18:14:39 +0200

    removed build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/dev/thecherno/vstudio
-# Build directory: /home/mike/dev/thecherno/vstudio/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
