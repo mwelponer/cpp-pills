478765271d7876612f18151781d93ffdb2f557d1
mike <welponer@fbk.eu>
2023-05-24 11:32:40 +0200

    switched to cmake till 9_const

--- 8_strings/strings.cpp
+++ 8_strings/strings.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string.h>
+#include <string.h> // for strlen and strcpy
 
 /* passing the string around functions*/
 void wrongPrintString(std::string string)
@@ -25,14 +25,15 @@ int main(int argc, char **argv)
 	 * change the lenght of the string, or the value of any of 
 	 * its characters so usually it is declared const */
 	const char* name = "Mike";
-	std::cout << name << std::endl;
-	std::cout << strlen(name) << std::endl;
+	std::cout << name << ", " << strlen(name) << std::endl;
 	
-	char* str1 = "Ciao";
+	const char* str1 = "Ciao";
+	std::cout << str1 << ", " << strlen(str1) << std::endl;
 	char str2[40];
 	strcpy(str2, str1); // copy str1 into str2 
-	std::cout << str2 << std::endl;
+	std::cout << str2 << ", " << strlen(str2) << std::endl;
 	
+
 	std::cout << "######### string manually initialized" << std::endl;
 	/* if we want to declare it completely manually 
 	 * we need to add the string termination char (0 or '\0') that 
@@ -40,7 +41,6 @@ int main(int argc, char **argv)
 	char another[5] = {'M', 'i', 'k', 'e', 0};
 	std::cout << another << std::endl;
 	
-
 
 	std::cout << "######### std::string" << std::endl;
 	/* finally if we want to use the standard string implementation.
