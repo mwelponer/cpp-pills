c434d037b09f958ac44efb5de54b71277e16e024
mike <welponer@fbk.eu>
2023-06-14 11:53:54 +0200

    functionPointers update

--- 21_functionPointers/functionPointers.cpp
+++ 21_functionPointers/functionPointers.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-void printValue(int value){
+void print(int value){
 	std::cout << "value: " << value << std::endl;
 }
 
@@ -11,11 +11,28 @@ void foreach(const std::vector<int>& values, void(*func)(int)){
 }
 
 int main(){
+
+    // let's try to create a function pointer: 
+    // 1. let's try to assign fptr the mem address of function print()
+    //auto fptr = &printValue;
+    //fptr;
+    //fptr; 
+    // it works..
+
+    // 2. what kind of type is auto? auto is void(*fptr)(int) so
+    //void(*fptr)(int) = print;
+    
+    // 3. or even better using typedef 
+    typedef void(*fptr)(int);
+    fptr myprint = print;
+    print(666);
+
+
 	std::vector<int> values = {1, 2, 3};
-    // so here I use foreach function to which I pass a vector of elements 
+    // here I use foreach function to which I pass a vector of elements 
     // and a function pointer: the function the pointer points 
     // to defines what I want to do with each element of the vector.
-	foreach(values, printValue);
+	foreach(values, print);
 
     // or using a lambda function
     foreach(values, [](int value){std::cout << "lvalue: " << value << std::endl;});
