e7d592ab913ecb56471632366fd81dc1c022b776
mike <mwelponer@gmail.com>
2023-07-24 19:13:43 +0200

    readme update, fixed explicit in implicit conversion code

--- 13_implicitConversion/implicitConversion.cpp
+++ 13_implicitConversion/implicitConversion.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Entity 
 {
 private:
 	std::string m_name;
 	int m_age;
-	float m_height;
+	std::vector<int> m_vector;
+
 public:
 	Entity(const std::string& name) 
 		: m_name(name), m_age(-1) {}
@@ -14,8 +16,8 @@ public:
 	Entity(int age) 
 		: m_name("unknown"), m_age(age) {}
 
-	explicit Entity(float height)
-		: m_name("unknown"), m_height(height) {}
+	explicit Entity(std::vector<int>& vector)
+		: m_name("unknown"), m_vector(vector) {}
 
 	std::string getName() const 
 	{ 
@@ -35,13 +37,13 @@ int main()
 	Entity b(43); 
 	// these are equivalent to 
 	Entity another_a = std::string("Mike");
-	Entity another_b = 43;
+	//Entity another_b = 43;
 	
 	// implicit conversion: 22 was implicitly converted into 
 	// an Entity with age 22
 	// c++ does the implicit conversion automagically only if there is not 
 	// more than 1 conversion to do between the two types
-	PrintEntity(22); 
+	PrintEntity(22);
 	
 	// implicit conversion: "Cherno" was firstly casted from 
 	// a const char array to a std string then implicitly converted 
@@ -51,8 +53,15 @@ int main()
 	PrintEntity(Entity("Cherno"));
 
 
-	// explicit keyword in front of the contructor deisable any form of implicit conversion
-	float height = 172.5f;
-	Entity another_c = height;
+	// explicit keyword in front of the contructor disable any form of implicit conversion
+	std::vector<int> vector = {1, 2, 3};
+
+	// this will give compilation error !!!
+	// PrintEntity(vector); 
+
+	// we need to explicitly say
+	Entity another_c(vector);
 	PrintEntity(another_c);
+	// or directly
+	PrintEntity(Entity(vector));
 }
